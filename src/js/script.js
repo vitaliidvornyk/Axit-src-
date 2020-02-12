b7983fe62f46b035e37da309468f4ca642360d81
vitaliidvornyk <59489402+vitaliidvornyk@users.noreply.github.com>
2020-02-12 21:57:48 +0200

    3 section

---
+++ src/js/script.js
@@ -0,0 +1,9 @@
+(function($) {
+    $(function() {     
+      $('ul.sentence__tabs').on('click', 'li:not(.sentence__tab_active)', function() {
+        $(this)
+          .addClass('sentence__tab_active').siblings().removeClass('sentence__tab_active')
+          .closest('div.container').find('div.sentence__content').removeClass('sentence__content_active').eq($(this).index()).addClass('sentence__content_active');
+      });     
+    });
+})(jQuery);
