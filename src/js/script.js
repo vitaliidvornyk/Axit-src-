4a37952ca759be0a982d8ddca3f9456ce3fff4f5
vitaliidvornyk <59489402+vitaliidvornyk@users.noreply.github.com>
2020-02-25 12:49:28 +0200

    adaptation_full

--- src/js/script.js
+++ src/js/script.js
@@ -10,6 +10,26 @@
     });
 })(jQuery);
 
+// Hamburger
+
+window.addEventListener('DOMContentLoaded', () => {
+  const menu = document.querySelector('.navbar__menu'),
+  menuItem = document.querySelectorAll('.navbar__menu_item'),
+  hamburger = document.querySelector('.hamburger');
+
+  hamburger.addEventListener('click', () => {
+      hamburger.classList.toggle('hamburger_active');
+      menu.classList.toggle('navbar__menu_active');
+  });
+
+  menuItem.forEach(item => {
+      item.addEventListener('click', () => {
+          hamburger.classList.toggle('hamburger_active');
+          menu.classList.toggle('navbar__menu_active');
+      })
+  })
+})
+
 // Scroll up
 
 // $(window).on( 'load', function() {
