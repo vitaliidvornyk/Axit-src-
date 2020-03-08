3bf0d5ead34ce7ae01bdd0dcf47c96dda17b9afe
Vitalii Dvornyk <dvornikvitaliy@gmail.com>
2020-03-08 10:22:41 +0200

    page up script

--- src/js/script.js
+++ src/js/script.js
@@ -1,37 +1,56 @@
+
 // Tabs
 
-(function($) {
-    $(function() {     
-      $('ul.sentence__tabs').on('click', 'li:not(.sentence__tab_active)', function() {
-        $(this)
-          .addClass('sentence__tab_active').siblings().removeClass('sentence__tab_active')
-          .closest('div.container').find('div.sentence__content').removeClass('sentence__content_active').eq($(this).index()).addClass('sentence__content_active');
-      });     
-    });
-})(jQuery);
+$(document).ready(function () {
+	$('ul.sentence__tabs').on('click', 'li:not(.sentence__tab_active)', function () {
+		$(this)
+			.addClass('sentence__tab_active').siblings().removeClass('sentence__tab_active')
+			.closest('div.container').find('div.sentence__content').removeClass('sentence__content_active').eq($(this).index()).addClass('sentence__content_active');
+	});
 
-// Hamburger
+	// Hamburger
 
-window.addEventListener('DOMContentLoaded', () => {
-  const menu = document.querySelector('.navbar__menu'),
-  menuItem = document.querySelectorAll('.navbar__menu_item'),
-  hamburger = document.querySelector('.hamburger');
+	window.addEventListener('DOMContentLoaded', () => {
+		const menu = document.querySelector('.navbar__menu'),
+			menuItem = document.querySelectorAll('.navbar__menu_item'),
+			hamburger = document.querySelector('.hamburger');
 
-  hamburger.addEventListener('click', () => {
-      hamburger.classList.toggle('hamburger_active');
-      menu.classList.toggle('navbar__menu_active');
-  });
+		hamburger.addEventListener('click', () => {
+			hamburger.classList.toggle('hamburger_active');
+			menu.classList.toggle('navbar__menu_active');
+		});
 
-  menuItem.forEach(item => {
-      item.addEventListener('click', () => {
-          hamburger.classList.toggle('hamburger_active');
-          menu.classList.toggle('navbar__menu_active');
-      })
-  })
-})
+		menuItem.forEach(item => {
+			item.addEventListener('click', () => {
+				hamburger.classList.toggle('hamburger_active');
+				menu.classList.toggle('navbar__menu_active');
+			})
+		})
+	})
 
-// Scroll up
+	// Scroll up
 
-$(window).on( 'load', function() {
-  $('html, body').animate({scrollTop : 0});
+	$(window).on('load', function () {
+		$('html, body').animate({ scrollTop: 0 });
+	});
+
+	// Page up
+
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > 1600) {
+			$('.pageup').fadeIn();
+		} else {
+			$('.pageup').fadeOut();
+		}
+	});
+
+	$(function () {
+		$("a[href=#up]").click(function () {
+			const _href = $(this).attr("href");
+			$("html, body").animate({ scrollTop: $(_href).offset().top + "px" });
+			return false;
+		});
+	});
+
 });
+
